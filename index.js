7fe9cfe184aa67b802ee1b28f67e3813169d1800
DarshTank <darshtank05@gmail.com>
2024-11-08 19:16:17 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send("Hello World!")
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('darshtank05@gmail.com')
+})
+
+app.get('/login',(req,res) => {
+    res.send('<h1>Hello! You are on the Login Page</h1>')
+})
+
+app.get('/move',(req,res)=>{
+    res.send("<a href='http://localhost:3000/login'>Click here To go For Login</a>")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example on port ${port}`)
+})
